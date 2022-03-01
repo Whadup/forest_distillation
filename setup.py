39232c935ddf86d1961b766ecd0add85bf5f61b1
Lukas Pfahler <lukas.pfahler@udo.edu>
2022-03-01 18:23:30 +0100

    requirements.txt

--- setup.py
+++ setup.py
@@ -25,25 +25,24 @@ ext_modules = [
 ]
 
 setup(
-    name='random_forest_robustness', # TODO: rename. 
+    name='random_forest_robustness',
     version=_VERSION,
-    description='An empty project base.',
+    description='Approximate a sklearn RandomForestClassifier using a single Decision Tree.',
     long_description=_README_MD,
     classifiers=[
-        # TODO: typing.
-        "Typing :: Typed"
     ],
-    url='https://github.com/Whadup/random_forest_distillation',  # TODO.
-    download_url='https://github.com/Whadup/random_forest_distillation/tarball/{}'.format(_VERSION),  # TODO.
-    author='Lukas Pfahler',  # TODO.
-    author_email='lukas.pfahler@tu-dortmund.de',  # TODO.
-    packages=find_packages(include=['random_forest_distillation*']),  # TODO.
+    url='https://github.com/Whadup/random_forest_distillation',  
+    download_url='https://github.com/Whadup/random_forest_distillation/tarball/{}'.format(_VERSION), 
+    author='Lukas Pfahler', 
+    author_email='lukas.pfahler@tu-dortmund.de',  
+    packages=find_packages(include=['random_forest_distillation*']),  
     test_suite="testing",
-    setup_requires=["pytest-runner"],
+    setup_requires=["pytest-runner", "Cython", "scikit-learn>=1.0", "numpy"],
     ext_modules = cythonize(ext_modules, language_level = "3"),
     tests_require=["pytest", "pytest-cov"],
     include_package_data=True,
-    license='MIT',  
-    keywords='empty project TODO keywords'
+    license='MIT',
+    python_requires=">3.7",
+    keywords='scikit-learn random forest to decision tree distillation'
 )
 
