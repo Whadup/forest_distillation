ab10ec1c7618404bf8283e05ade4c51f2f1a358a
Lukas Pfahler <lukas.pfahler@udo.edu>
2022-03-02 10:23:45 +0100

    changes

--- test_moo.py
+++ test_moo.py
@@ -22,6 +22,81 @@ from random_forest_distillation import distill
 from random_forest_distillation.utils import model_size, consume_extra_arguments, type_guess
 from meticulous import Experiment
 
+from pymoo.core.mutation import Mutation
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.optimize import minimize
+from pymoo.core.sampling import Sampling
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.crossover import Crossover
+
+class Forest():
+    def __init__(self, X):
+        self.X = X
+
+
+class MyCrossover(Crossover):
+    def __init__(self):
+
+        # define the crossover: number of parents and number of offsprings
+        super().__init__(2, 2)
+
+    def _do(self, problem, X, **kwargs):
+        print("CROSSOVER")
+        Y = X
+        for k in range(X.shape[1]):
+            Y[0, k, 0].X.estimators_ = list(np.random.choice(list(X[0,k,0].X.estimators_) + list(X[1,k,0].X.estimators_), size=(X[0,k,0].X.n_estimators + X[1,k,0].X.n_estimators) // 2))
+            Y[0, k, 0].X.n_estimators = len(Y[0, k, 0].X.estimators_)
+        # print(Y)
+        return Y
+
+
+
+class InitSampling(Sampling):
+    def __init__(self, start):
+        super().__init__()
+        self.start = start
+
+    def _do(self, problem, n_samples, **kwargs):
+        X = []
+        for i in range(n_samples):
+            X.append([Forest(deepcopy(self.start))])
+        print("init", X)
+        return X
+
+class MyProblem(ElementwiseProblem):
+    def __init__(self, X_test, y_test):
+        super().__init__(n_var=1, n_obj=2, n_constr=0)
+        self.X_test = X_test
+        self.y_test = y_test
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        print("eval", x[0])
+        x = x[0].X
+        acc = accuracy_score(self.y_test, x.predict(self.X_test))
+        size = model_size(x)
+        out["F"] = np.array([-acc, size], dtype=float)
+
+class DoSomething(Mutation):
+    def __init__(self):
+        super().__init__()
+
+    def _do(self, problem, X, **kwargs):
+        new_X = []
+        for i in range(len(X)):
+            while True:
+                operation = np.random.choice(["distill", "prune"])
+                if operation == "distill":
+                    if np.random.random() < 0.5:
+                        max_nodes = np.random.randint(2, 128)
+                        new_X.append([Forest(distill(X[i,0].X, max_nodes=max_nodes, return_forest=True))])
+                        break
+                    else:
+                        max_depth = np.random.randint(1, 6)
+                        new_X.append([Forest(distill(X[i,0].X, max_depth=max_depth, return_forest=True))])
+                        break
+                elif operation == "prune":
+                    continue
+        return new_X
 
 
 def get_data(dataset="MagicTelescope", quantile_transform=True):
@@ -81,3 +156,22 @@ if __name__ == "__main__":
     print("PROXY ACCURACY", proxy_acc)
     experiment.summary(proxy_accuracy=proxy_acc)
 
+
+
+
+    algorithm = NSGA2(pop_size=32,
+                  sampling=InitSampling(model),
+                  crossover=MyCrossover(),
+                  mutation=DoSomething(),
+                  eliminate_duplicates=None)
+    res = minimize(MyProblem(X_test, y_test),
+               algorithm,
+               ('n_gen', 128),
+               seed=1,
+               save_history=True,
+               verbose=True)
+    print(res)
+    print(res.X)
+    print(res.F)
+    if len(extra_args):
+        print("WARNING! Unused command line arguments remaining:", extra_args)
